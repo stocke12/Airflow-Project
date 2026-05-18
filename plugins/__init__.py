dc7694fa791ee77c5badd1cbf98807c83ff816dc
Stockton Erickson <143432670+stocke12@users.noreply.github.com>
2026-05-18 09:26:30 -0600

    Add UdacityPlugin for Airflow integration
    Created a new Airflow plugin with custom operators and helpers.

---
+++ plugins/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import division, absolute_import, print_function
+
+from airflow.plugins_manager import AirflowPlugin
+
+# Explicitly import your packages from the local directory
+from operators import (StageToRedshiftOperator, LoadFactOperator, 
+                       LoadDimensionOperator, DataQualityOperator, CreateTableOperator)
+from helpers import SqlQueries
+
+# Defining the plugin class
+class UdacityPlugin(AirflowPlugin):
+    name = "udacity_plugin"
+    operators = [
+        StageToRedshiftOperator,
+        LoadFactOperator,
+        LoadDimensionOperator,
+        DataQualityOperator,
+        CreateTableOperator
+    ]
+    helpers = [
+        SqlQueries
+    ]
